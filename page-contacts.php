4bf622aeca0931eeb244b67dae56578dbb83b509
tarchevsky <i.tarchevsky@gmail.com>
2024-03-27 09:55:42 +0300

    Начало работы с контактами, вставил вопросы клиента в faq

--- page-contacts.php
+++ page-contacts.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 
-<main class="min-h-[600px] bg-base-200">
-    <div class="hero-content m-auto text-center">
-        <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Какой-то заглавный текст</h1>
-            <button class="btn btn-lg btn-accent sm:w-full">Записаться</button>
-        </div>
+<main class="cont flex items-center h-[20vh]">
+    <div class="">
+        <h1 class="text-4xl font-bold">Контакты</h1>
     </div>
+
 </main>
 
 <?php get_footer(); ?>
