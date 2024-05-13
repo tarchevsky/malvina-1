7c49ff8cec9e7b054c433f5a42a757bf01eeae46
tarchevsky <i.tarchevsky@gmai.com>
2024-05-13 19:37:19 +0300

    контейнер для вывода постов в page-blog

--- page-blog.php
+++ page-blog.php
@@ -11,43 +11,45 @@
 <div class="cont mt-80">
     <?php the_content(); ?>
 </div>
-<?php
-$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-$args = array(
-    'post_type' => 'post',
-    'category_name' => 'blog',
-    'posts_per_page' => 10,
-    'paged' => $paged
-);
-
-$query = new WP_Query( $args );
-
-if ( $query->have_posts() ) {
-    while ( $query->have_posts() ) {
-        $query->the_post();
-        ?>
-        <h2><?php the_title(); ?></h2>
-        <?php if ( has_post_thumbnail() ) {
-            the_post_thumbnail();
-        } ?>
-        <p><?php the_excerpt(); ?></p>
-        <?php
-    }
-    ?>
-    <div class="pagination">
-        <?php
-        echo paginate_links( array(
-            'total' => $query->max_num_pages
-        ) );
-        ?>
-    </div>
+<div class="cont">
     <?php
-} else {
-    // Если нет постов
-    echo 'Постов не найдено.';
-}
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+    $args = array(
+        'post_type' => 'post',
+        'category_name' => 'blog',
+        'posts_per_page' => 10,
+        'paged' => $paged
+    );
 
-wp_reset_postdata();
-?>
+    $query = new WP_Query( $args );
+
+    if ( $query->have_posts() ) {
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            ?>
+            <h2><?php the_title(); ?></h2>
+            <?php if ( has_post_thumbnail() ) {
+                the_post_thumbnail();
+            } ?>
+            <p><?php the_excerpt(); ?></p>
+            <?php
+        }
+        ?>
+        <div class="pagination">
+            <?php
+            echo paginate_links( array(
+                'total' => $query->max_num_pages
+            ) );
+            ?>
+        </div>
+        <?php
+    } else {
+        // Если нет постов
+        echo 'Постов не найдено.';
+    }
+
+    wp_reset_postdata();
+    ?>
+</div>
 
 <?php get_footer(); ?>
